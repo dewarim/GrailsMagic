be63e9291568204f40deff0941a8c760d3d75d83
Ingo Wiarda <ingo_wiarda@dewarim.de>
2014-02-22 19:13:05 +0100

    for testing: add code to output mkmApi-Key. Currently disabled again.

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,15 @@
 class BootStrap {
 
+    def grailsApplication
+    
     def init = { servletContext ->
+        
+        environments{
+            development{
+//                log.debug("mkmApiKey: "+grailsApplication.config.mkmApiKey)
+            }
+        }
+        
     }
     def destroy = {
     }
